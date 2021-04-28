9d5a943094351481e2519644c4721a7e468844e9
spirithacodesh <neguedayinhara@gmail.com>
2021-04-29 00:17:48 +0300

    finish

--- src/components/CV/Body/Body.js
+++ src/components/CV/Body/Body.js
@@ -10,27 +10,27 @@ const Body = () => {
 
                 <div className="slider0">
                     <p className="taillePAside font-weight-bold">{t('Body.2')}</p>
-                    <p className="taillePAside">{t('Body.3')}</p>
-                    <p className="taillePAside">{t('Body.4')}</p>
-                    <p className="taillePAside">{t('Body.5')}</p>
-                    <p className="taillePAside">{t('Body.6')}</p>
-                    <p className="taillePAside">{t('Body.7')}</p>
-                    <p className="taillePAside mb-4">{t('Body.8')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span> {t('Body.3')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.4')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.5')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.6')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.7')}</p>
+                    <p className="taillePAside mb-4"><span className="text-danger mr-1">&Phi;</span>{t('Body.8')}</p>
                 </div>
                 
                 <div className="slider1">
                     <p className="taillePAside font-weight-bold">{t('Body.9')}</p>
-                    <p className="taillePAside">{t('Body.10')}</p>
-                    <p className="taillePAside">{t('Body.11')}</p>
-                    <p className="taillePAside">{t('Body.12')}</p>
-                    <p className="taillePAside mb-4">{t('Body.13')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.10')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.11')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.12')}</p>
+                    <p className="taillePAside mb-4"><span className="text-danger mr-1">&Phi;</span>{t('Body.13')}</p>
                 </div>
 
                 <div className="slider2">
                     <p className="taillePAside font-weight-bold">{t('Body.14')}</p>
-                    <p className="taillePAside">{t('Body.15')}</p>
-                    <p className="taillePAside">{t('Body.16')}</p>
-                    <p className="taillePAside">{t('Body.17')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.15')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.16')}</p>
+                    <p className="taillePAside"><span className="text-danger mr-1">&Phi;</span>{t('Body.17')}</p>
                 </div>
 
             </div>
